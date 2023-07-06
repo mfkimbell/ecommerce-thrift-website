6ce1a86ff07d184be56075979fec7f7d8ddff59d
betty <65417079+bettychen87@users.noreply.github.com>
2023-07-06 15:18:42 -0400

    finished second half

--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import styled from 'styled-components';
+import { Badge } from "@mui/material";
 import { Search, ShoppingCartOutlined } from "@mui/icons-material";
-import { Badge } from '@mui/material';
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-    height: 60px;
+  height: 60px;
 `;
 
 const Wrapper = styled.div`
-    padding: 10px 20px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
+
 const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
 `;
+
 const Language = styled.span`
-  font-size = 14px;
+  font-size: 14px;
   cursor: pointer;
 `;
 
@@ -29,21 +31,22 @@ const SearchContainer = styled.div`
   align-items: center;
   margin-left: 25px;
   padding: 5px;
-`
+`;
 
 const Input = styled.input`
   border: none;
-`
+`;
+
 const Center = styled.div`
-  flex: 1
+  flex: 1;
   text-align: center;
 `;
 
 const Logo = styled.h1`
   font-weight: bold;
-`
+`;
 const Right = styled.div`
-  flex: 1
+  flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
@@ -52,34 +55,35 @@ const Right = styled.div`
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
-  margin-left: 25px
-`
+  margin-left: 25px;
+`;
+
 const Navbar = () => {
   return (
     <Container>
-        <Wrapper>
-            <Left>
-              <Language>EN</Language>
-              <SearchContainer>
-                <Input/>
-                <Search/>
-              </SearchContainer>
-            </Left>
-            <Center>
-              <Logo>LAMA.</Logo>
-            </Center>
-            <Right>
-              <MenuItem>REGISTER</MenuItem>
-              <MenuItem>SIGN IN</MenuItem>
-              <MenuItem>
-                <Badge badgeContent={4} color="primary">
-                  <ShoppingCartOutlined />
-                </Badge>
-              </MenuItem>
-            </Right>
-        </Wrapper>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search" />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>LAMA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
