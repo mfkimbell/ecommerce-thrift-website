a990b24cdaacea984acc32edfb3d5e657905ea83
Andrew Lewis <lewis@tricorderarray.com>
2023-07-11 13:57:01 -0500

    Fix: Moved Announcement to the top of the page

--- myapp/src/pages/ProductList.jsx
+++ myapp/src/pages/ProductList.jsx
@@ -35,8 +35,8 @@ const Option = styled.option``;
 const ProductList = () => {
   return (
     <Container>
-      <Navbar />
       <Announcement />
+      <Navbar />
       <Title>Dresses</Title>
       <FilterContainer>
         <Filter>
