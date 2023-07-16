115c3c4ef68f7ea2e4f2a5f8fba201db4fa41041
mitchell_kimbell <mfkimbell@gmail.com>
2023-07-16 04:43:44 -0500

    Add mongo db models

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-  res.send("user test is sucessful");
-});
+// router.get("/usertest", (req, res) => {
+//   res.send("user test is sucessful");
+// });
 
-router.post("/userposttest", (req, res) => {
-  const username = req.body.username;
-  console.log(username);
-});
+// router.post("/userposttest", (req, res) => {
+//   const username = req.body.username;
+//   console.log(username);
+// });
+
+
+
 
 module.exports = router;
