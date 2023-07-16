ca6b37100730d8976eb48e792b522a459977551d
mitchell_kimbell <mfkimbell@gmail.com>
2023-07-16 03:56:15 -0500

    setup MongoDB connection

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connection successful"))
+  .catch((err) => console.log("error: DB connection was un-sucessful"));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running! :)");
+});
