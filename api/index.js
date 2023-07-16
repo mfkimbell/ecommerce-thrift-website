d509c8804b2a9b76b23e26d28efe6c69b375a7c6
mitchell_kimbell <mfkimbell@gmail.com>
2023-07-16 04:19:04 -0500

    sucessfully query routes and connect them to functions

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
 dotenv.config();
 
@@ -9,6 +10,13 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB connection successful"))
   .catch((err) => console.log("error: DB connection was un-sucessful"));
+
+app.get("/api/test", () => {
+  console.log("test");
+});
+
+app.use(express.json());
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running! :)");
