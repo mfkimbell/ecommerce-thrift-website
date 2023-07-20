1e018c921f4847afc2fe6d38a4832d492592d2af
om_patel <ompatel@uab.edu>
2023-07-20 07:12:48 -0500

    changed product schema and added page routes

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -4,9 +4,28 @@ import ProductList from "./pages/ProductList";
 import Login from "./pages/Login";
 import Register from "./pages/Register"
 import Cart from "./pages/Cart"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
 
 const App = () => {
-  return <Home />;
+  const user = true
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
