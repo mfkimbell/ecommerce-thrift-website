c1dcc7c0cd799460b69d6a16cd8092be390ab5f5
mitchell_kimbell <mfkimbell@gmail.com>
2023-08-04 15:58:07 -0500

    updated contacts

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,6 +1,6 @@
 import Home from "./pages/Home";
 import Product from "./pages/Product";
-import ProductList from "./pages/ProductList";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register"
 import Cart from "./pages/Cart"
@@ -20,9 +20,7 @@ const App = () => {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/products/:category">
-          <ProductList />
-        </Route>
+       
         <Route path="/product/:id">
           <Product />
         </Route>
